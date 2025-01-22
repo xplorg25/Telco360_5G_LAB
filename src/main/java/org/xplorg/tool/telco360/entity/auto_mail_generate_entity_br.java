175a6d2aea8d07635e2e4233c3dd295ca00ce258
xplorg25 <164178475+xplorg25@users.noreply.github.com>
2025-01-22 17:30:46 +0530

    Add files via upload

---
+++ src/main/java/org/xplorg/tool/telco360/entity/auto_mail_generate_entity_br.java
@@ -0,0 +1,108 @@
+package org.xplorg.tool.telco360.entity;
+
+public class auto_mail_generate_entity_br {
+
+String admin_id;
+String report_name;
+String report_duration;
+String report_interval;
+String start_date;
+String end_date;
+String start_time;
+String end_time;
+String domain;
+String vendor;
+String element;
+String save_report;
+String single_multiple;
+String mail;
+public String getMail() {
+	return mail;
+}
+public void setMail(String mail) {
+	this.mail = mail;
+}
+public String getSingle_multiple() {
+	return single_multiple;
+}
+public void setSingle_multiple(String single_multiple) {
+	this.single_multiple = single_multiple;
+}
+public String getSave_report() {
+	return save_report;
+}
+public void setSave_report(String save_report) {
+	this.save_report = save_report;
+}
+public String getReport_duration() {
+	return report_duration;
+}
+public void setReport_duration(String report_duration) {
+	this.report_duration = report_duration;
+}
+public String getAdmin_id() {
+	return admin_id;
+}
+public void setAdmin_id(String admin_id) {
+	this.admin_id = admin_id;
+}
+public String getReport_name() {
+	return report_name;
+}
+public void setReport_name(String report_name) {
+	this.report_name = report_name;
+}
+
+public String getReport_interval() {
+	return report_interval;
+}
+public void setReport_interval(String report_interval) {
+	this.report_interval = report_interval;
+}
+public String getStart_date() {
+	return start_date;
+}
+public void setStart_date(String start_date) {
+	this.start_date = start_date;
+}
+public String getEnd_date() {
+	return end_date;
+}
+public void setEnd_date(String end_date) {
+	this.end_date = end_date;
+}
+public String getStart_time() {
+	return start_time;
+}
+public void setStart_time(String start_time) {
+	this.start_time = start_time;
+}
+public String getEnd_time() {
+	return end_time;
+}
+public void setEnd_time(String end_time) {
+	this.end_time = end_time;
+}
+public String getDomain() {
+	return domain;
+}
+public void setDomain(String domain) {
+	this.domain = domain;
+}
+public String getVendor() {
+	return vendor;
+}
+public void setVendor(String vendor) {
+	this.vendor = vendor;
+}
+public String getElement() {
+	return element;
+}
+public void setElement(String element) {
+	this.element = element;
+}
+
+
+
+
+}
