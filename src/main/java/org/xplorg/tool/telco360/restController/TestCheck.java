0abebcc413eace25fbc484222a25b1c124f7782f
root <root@tooldocumentation>
2025-01-22 17:57:59 +0530

    ic

---
+++ src/main/java/org/xplorg/tool/telco360/restController/TestCheck.java
@@ -0,0 +1,14 @@
+package org.xplorg.tool.telco360.restController;
+
+public class TestCheck {
+
+	public TestCheck() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
